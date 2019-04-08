4e703a2f13a88fab608de5886c2a0dc8581c1ab8
李伟 <1416017757@qq.com>
2019-04-08 18:24:01 +0800

    test

---
+++ math/min.js
@@ -0,0 +1,20 @@
+let amount = 3399,
+    days = 10,
+    min = 100,
+    max = 1000;
+
+
+let minCount = 0, // 最多还多少次
+    maxCount = 0; // 最少还多少次
+
+// 逻辑
+maxCount = Math.floor(amount / min);
+minCount = amount / max
+
+
+
+
+
+// 结果
+console.log(maxCount)
+console.log(minCount)
